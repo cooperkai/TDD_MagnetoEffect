21966e3898eab434a2151fa3ebed5a79403177a9
cooperkai <eatkaikai@gmail.com>
2023-04-03 22:52:52 +0800

    add junit5 for testing framework

---
+++ src/test/java/com/cooper/magneteffect/MagnetEffectTest.java
@@ -0,0 +1,14 @@
+package com.cooper.magneteffect;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MagnetEffectTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
