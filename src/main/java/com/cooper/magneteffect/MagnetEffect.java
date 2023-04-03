993982f59e449790cd24476259ade29e2b7b3061
cooperkai <eatkaikai@gmail.com>
2023-04-04 02:42:10 +0800

    red light: check method return the same position for quick pass the test, but it still not pass

--- src/main/java/com/cooper/magneteffect/MagnetEffect.java
+++ src/main/java/com/cooper/magneteffect/MagnetEffect.java
@@ -3,8 +3,7 @@ package com.cooper.magneteffect;
 public class MagnetEffect {
 
 	public Point check(Point beforePoint) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Point(49, 49);
 	}
 
 }
--- src/main/java/com/cooper/magneteffect/Point.java
+++ src/main/java/com/cooper/magneteffect/Point.java
@@ -1,9 +1,17 @@
 package com.cooper.magneteffect;
 
 public class Point {
-
+	private int x;
+	private int y;
+	
 	public Point(int x, int y) {
-		// TODO Auto-generated constructor stub
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
 	}
 
 }
