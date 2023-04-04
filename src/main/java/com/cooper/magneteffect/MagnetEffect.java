c7e2a15915075bfb0118c351f2d8acc19820192b
cooperkai <eatkaikai@gmail.com>
2023-04-05 01:38:35 +0800

    green light: refactory the addAnchor method: using list to save multiple anchors、check method: use the list to find the near anchor、isInCircle: add anchor as parameter

--- src/main/java/com/cooper/magneteffect/MagnetEffect.java
+++ src/main/java/com/cooper/magneteffect/MagnetEffect.java
@@ -1,25 +1,31 @@
 package com.cooper.magneteffect;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MagnetEffect {
 
-	private Point anchor;
+	private List<Point> anchors = new ArrayList<>();
 
 	public Point check(Point beforePoint) {
-		if (anchor == null) {
+		if (anchors.isEmpty()) {
 			return beforePoint;
 		} else {
-			if (isInCircle(beforePoint)) {
-				return anchor;
+			for (Point anchor : anchors) {
+				if (isInCircle(anchor, beforePoint)) {
+					return anchor;
+				}
+				return beforePoint;
 			}
-			return beforePoint;
 		}
+		return beforePoint;
 	}
 
 	public void addAnchor(Point anchor) {
-		this.anchor = anchor;
+		anchors.add(anchor);
 	}
 
-	private boolean isInCircle(Point beforePoint) {
+	private boolean isInCircle(Point anchor, Point beforePoint) {
 		int distance = (int) Math.sqrt((anchor.getX() - beforePoint.getX()) * (anchor.getX() - beforePoint.getX())
 				+ (anchor.getY() - beforePoint.getY()) * (anchor.getY() - beforePoint.getY()));
 		return distance <= 5;
