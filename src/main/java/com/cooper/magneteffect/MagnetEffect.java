cf03add4ac2227e6911040f47047dfb58433ae4c
cooperkai <eatkaikai@gmail.com>
2023-04-04 19:42:45 +0800

    green light: calculate an anchor and mouse position is in the circle

--- src/main/java/com/cooper/magneteffect/MagnetEffect.java
+++ src/main/java/com/cooper/magneteffect/MagnetEffect.java
@@ -8,12 +8,20 @@ public class MagnetEffect {
 		if (anchor == null) {
 			return beforePoint;
 		} else {
-			return anchor;
+			if (isInCircle(anchor.getX(), beforePoint.getX(), anchor.getY(), beforePoint.getY())) {
+				return anchor;
+			}
+			return beforePoint;
 		}
 	}
 
 	public void addAnchor(Point anchor) {
 		this.anchor = anchor;
+	}
+
+	private boolean isInCircle(int x, int centerX, int y, int centerY) {
+		int distance = (int) Math.sqrt((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY));
+		return distance <= 5;
 	}
 
 }
